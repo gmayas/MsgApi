61a21cd608e73e1e98db88159d3034fb6e320cd2
Gabriel Maya Sánchez. <isc_gmayas@hotmail.com>
2025-08-21 23:54:04 -0600

    VercelJson commit

--- src/js/database.js
+++ src/js/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 // Database connection
-const URIMONGODB = process.env.URIMONGODB; // Ensure this is set in your .env file
+const URIMONGODB = process.env.URIMONGODB || 
+"mongodb+srv://gmayasds:a2Amq1aL7GO2TIuq@clustermsg.fxdwzg2.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMsg"; // Ensure this is set in your .env file
 if (!URIMONGODB) {
   console.error('URIMONGODB is not defined in the environment variables.');
   process.exit(1);
