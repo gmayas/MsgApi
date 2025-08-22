4f126d01e273e9c3ae63b527560c2d6cc62ab85d
Gabriel Maya Sánchez. <isc_gmayas@hotmail.com>
2025-08-21 22:41:03 -0600

    second commit

--- src/js/database.js
+++ src/js/database.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 // Database connection
-const URIMONGODB = "mongodb+srv://gmayasds:a2Amq1aL7GO2TIuq@clustermsg.fxdwzg2.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMsg";
+const URIMONGODB = process.env.URIMONGODB; // Ensure this is set in your .env file
+if (!URIMONGODB) {
+  console.error('URIMONGODB is not defined in the environment variables.');
+  process.exit(1);
+}   
 //
 mongoose.connect(URIMONGODB, {})
-.then((db) => console.log("Mongodb is connected to: ", URIMONGODB))
+.then((db) => console.log("Mongodb is connected successfully in Cluster: ", process.env.CLUSTER_NAME))
 .catch(err => console.log('Mongodb connected error in Cluster: ', err));
